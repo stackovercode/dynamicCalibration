aa028a888be09b8ddb0601dd61dd11e04065a064
stackovercode <reventlov@outlook.dk>
2021-11-03 04:04:30 -0700

    Added classes

---
+++ src/myHTTPClient.cpp
@@ -0,0 +1,73 @@
+#include "myHTTPClient.h"
+
+MyHTTPClient::MyHTTPClient(std::string ip, int port)
+{
+    mHost = ip + ":";
+    mHost += std::to_string(port);
+    std::cout << "<HTTP CLIENT CREATED>" << std::endl;
+    curlpp::Cleanup cleanup;
+}
+
+bool MyHTTPClient::begin()
+{
+    mRequest.reset();
+    std::stringstream ss;
+    std::string path = "/begin?";
+    mRequest.setOpt<curlpp::options::Url>("http://" + mHost + path);
+    mRequest.setOpt<curlpp::options::WriteStream>(&ss);
+    mRequest.perform();
+    std::string response;
+    response = ss.str();
+    if (response == "yes")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void MyHTTPClient::sendTransformationMatrix(std::string tMatrix)
+{
+    mRequest.reset();
+    std::string path = "/tMatrix?";
+    mRequest.setOpt<curlpp::options::Url>("http://" + mHost + path + tMatrix);
+    mRequest.perform();
+}
+
+void MyHTTPClient::sendCameraCalibrationMatrix(std::string ccMatrix)
+{
+    mRequest.reset();
+    std::string path = "/ccMatrix?";
+    mRequest.setOpt<curlpp::options::Url>("http://" + mHost + path + ccMatrix);
+    mRequest.perform();
+}
+
+void MyHTTPClient::sendPoints(std::array<std::string, 6> points)
+{
+    mRequest.reset();
+    std::string path = "/points?";
+    std::string variables = points[0] + "&" + points[1] + "&" +
+                            points[2] + "&" + points[3] + "&" +
+                            points[4] + "&" + points[5];
+    mRequest.setOpt<curlpp::options::Url>("http://" + mHost + path + variables);
+    std::cout << "URI: http://" + mHost + path + variables << std::endl;
+    mRequest.perform();
+}
+
+void MyHTTPClient::sendStatus(std::string status)
+{
+    mRequest.reset();
+    std::string path = "/status?";
+    mRequest.setOpt<curlpp::options::Url>("http://" + mHost + path + status);
+    mRequest.perform();
+}
+
+void MyHTTPClient::sendEndOfThrow()
+{
+    mRequest.reset();
+    std::string path = "/end?";
+    mRequest.setOpt<curlpp::options::Url>("http://" + mHost + path);
+    mRequest.perform();
+}
---
+++ src/myHTTPClient.h
@@ -0,0 +1,27 @@
+#ifndef MYHTTPCLIENT_H
+#define MYHTTPCLIENT_H
+#include <iostream>
+#include <sstream>
+#include <curlpp/cURLpp.hpp>
+#include <curlpp/Easy.hpp>
+#include <curlpp/Options.hpp>
+
+
+class MyHTTPClient
+{
+public:
+    MyHTTPClient(std::string ip, int port);
+
+    bool begin();
+    void sendTransformationMatrix(std::string tMatrix);
+    void sendCameraCalibrationMatrix(std::string ccMatrix);
+    void sendPoints(std::array<std::string, 6> points);
+    void sendStatus(std::string status);
+    void sendEndOfThrow();
+
+private:
+    std::string mHost;
+    curlpp::Easy mRequest;
+};
+
+#endif // MYHTTPCLIENT_H
